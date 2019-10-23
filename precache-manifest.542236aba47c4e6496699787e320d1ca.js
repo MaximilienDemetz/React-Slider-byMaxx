ca095d2cd2c4ea1922f3cb83628e814565944f31
Maximilien <max.demetz@hotmail.com>
2019-10-23 10:29:03 +0200

    Updates

---
+++ precache-manifest.542236aba47c4e6496699787e320d1ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa07c3864302ea5e76586c10c466f018",
+    "url": "/my-slider/index.html"
+  },
+  {
+    "revision": "24ae1d992db27c9bf06b",
+    "url": "/my-slider/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "f46d960a8c9c22ac43c6",
+    "url": "/my-slider/static/css/main.f37e20d2.chunk.css"
+  },
+  {
+    "revision": "24ae1d992db27c9bf06b",
+    "url": "/my-slider/static/js/2.36262224.chunk.js"
+  },
+  {
+    "revision": "f46d960a8c9c22ac43c6",
+    "url": "/my-slider/static/js/main.c0e43e0c.chunk.js"
+  },
+  {
+    "revision": "4c9e5b8e383f568f3aad",
+    "url": "/my-slider/static/js/runtime-main.5f98f693.js"
+  }
+]);
